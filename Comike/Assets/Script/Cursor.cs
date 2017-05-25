2fcc42fdbf41d1118aef5ee4288988873faf8ac7
nwuser <nwuser@da.iwasaki.ac.jp>
2017-05-25 17:20:55 +0900

    あああああああああああああああ！！！

---
+++ Comike/Assets/Script/Cursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            // 座標移動
+            
+        }
+    }
+}
