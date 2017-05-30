1cf1c175e44ddbd81c54d1427ea7b2e949d51eed
nwuser <nwuser@da.iwasaki.ac.jp>
2017-05-30 14:18:31 +0900

    カーソル―

--- Comike/Assets/Script/Cursor.cs
+++ Comike/Assets/Script/Cursor.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Cursor : MonoBehaviour {
 
+    int y;
+
 	// Use this for initialization
 	void Start () {
-		
+        transform.position = Vector3.zero;
+        transform.Translate(-5, -1, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) &&
+            transform.position.y == -1)
         {
             // 座標移動
+            transform.Translate(0, -3, 0);
             
         }
+        else if (Input.GetKey(KeyCode.UpArrow) &&
+            transform.position.y == -4)
+        {
+            // 座標移動
+            transform.Translate(0, 3, 0);
+        }
+
+        
+    }
+
+    public int ReturnCursorY()
+    {
+
+        return (int)transform.position.y;
     }
 }
